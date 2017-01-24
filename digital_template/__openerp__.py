7bc4de5d24d0c2ad0ba1df6c85d125e3d572fa87
odoo17 <odoo90@outlook.com>
2017-01-24 16:00:36 +0200

    template
    temp

---
+++ digital_template/__openerp__.py
@@ -0,0 +1,15 @@
+{
+    'name' : 'digital_template',
+    'version' : '0.1',
+    'author' : 'Digital Pages Developer',
+    'category' : 'Tools',
+    'website' : '',
+    'description': """
+Give Odoo 8.0 Digital Pages Style.
+    """,
+    'depends' : ['base'],
+    'data':[
+        'views.xml',
+        ],
+    'installable': True
+}
